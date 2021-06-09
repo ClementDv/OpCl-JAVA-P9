35edd1454ee4607f17e1b4e8d7b410ea2c1b80f3
Clément Poirier <p.clement777@gmail.com>
2021-06-09 15:37:34 +0200

    init

---
+++ src/test/java/com/mediscreen/mediscreenapp/MediscreenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mediscreenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
